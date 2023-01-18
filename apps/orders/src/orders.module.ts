f6fababcd45b80ec230d5598bbf506b2f0f1ffc5
caesarsage <destinyerhabor6@gmail.com>
2023-01-18 13:11:11 +0100

    Order App

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
-
+import * as Joi from 'joi'
+import { DatabaseModule } from '@app/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Order } from './schemas/order.schema';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: Order,
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
